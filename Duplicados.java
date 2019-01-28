cd608c6cf1a090ac7999dd3a93ba195ca9086dd8
Fernanda Zatarain <45245227+fdagz96@users.noreply.github.com>
2019-01-28 15:24:18 -0600

    Add files via upload

---
+++ Duplicados.java
@@ -0,0 +1,27 @@
+
+package duplicados;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class Duplicados {
+
+    public static void main(String[] args) {
+        
+        HashMap<Integer, String> arreglo = new HashMap<Integer, String>();
+                arreglo.putAll((Map<? extends Integer, ? extends String>) Arrays.asList(
+                "low", "disillusioned","decorate", "throat", "wrench",
+                "pack", "zip", "offbeat", "daughter", "kind", 
+                "confess", "swift", "part", "route", "spiffy", 
+                "groovy", "trail", "damaged", "satisfying", "oval", 
+                "decorous", "divergent", "hobbies", "disillusioned", "prevent",
+                "hypnotic", "supreme", "secretive", "company", "scarf",
+                "watch", "coach", "likeable", "rich", "wound", 
+                "endurable", "babies", "look", "tough", "jumpy", 
+                "grandmother", "lean", "omniscient", "explode", "cheerful", 
+                "bomb", "likeable","arrest", "arrest", "arrest")); 
+    
+
+    }
+}
